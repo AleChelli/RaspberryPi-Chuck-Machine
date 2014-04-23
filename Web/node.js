d62fae7b2243ad646558a9ad0fa9914c466e1d86
Alessandro Chelli <alessandrochelli@gmail.com>
2014-04-23 23:42:23 +0200

    Create the repository

---
+++ Web/node.js
@@ -0,0 +1,49 @@
+//Node modules for the upload of ck file and for launching different process
+var fs = require('fs')
+  , sys = require('sys')
+  , exec = require('child_process').exec
+  , util = require('util')
+  , Files = {};
+
+
+//Serve client side statically
+var http = require('http');
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname));
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+function puts(error, stdout, stderr) { 
+	sys.puts(stdout) 
+}
+
+
+io.sockets.on('connection', function (socket) {
+	exec("chuck --loop", puts);
+
+	socket.on('SendFile', function (data){
+		var file = data['File'];
+		fs.writeFile(__dirname +"/File/test.ck", file , function(err) {
+    	if(err) {
+        	console.log(err);
+    	} else {
+        	exec("chuck + File/test.ck", puts);
+    	}
+});
+	});
+});
